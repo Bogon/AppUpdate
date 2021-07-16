a462170e2bd1baa969b599bec178c406d8f5f176
香楠 <zhangqixcu@gmail.com>
2021-07-16 21:28:43 +0800

    release 0.1.0

---
+++ publish.sh
@@ -0,0 +1,30 @@
+
+# 提交修改，推到主分支
+echo '++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
+git add .
+git commit -m "release 0.1.0"
+git push -u origin main
+
+echo '+++++++++++++++++++++++++++ commit successful ++++++++++++++++++++'
+
+# 注意需要修改当前的版本号
+echo '+++++++++++++++++++++++++++ pushing tags ++++++++++++++++++++'
+git tag 0.1.0
+git push --tags
+
+echo '+++++++++++++++++++++++++++ pushing successful ++++++++++++++++++++'
+
+# 检查库的语法完整性
+echo '+++++++++++++++++++++++++++ spec lint ++++++++++++++++++++'
+pod spec lint --allow-warnings
+
+echo '+++++++++++++++++++++++++++ spec lint successful ++++++++++++++++++++'
+
+# 提交仓库到本地和公共仓库
+echo '+++++++++++++++++++++++++++ push spec ++++++++++++++++++++'
+pod repo add AppUpdate https://github.com/Bogon/CCSpec
+pod repo add AppUpdate https://github.com/CocoaPods/Specs
+pod repo push AppUpdate AppBaseKit.podspec --sources='https://github.com/Bogon/CCSpec.git, https://github.com/CocoaPods/Specs.git' --allow-warnings
+
+echo '+++++++++++++++++++++++++++ push spec successful ++++++++++++++++++++'
+
