59f30ccad902c44e43fb18d25f53a1f8ed1d2e40
香楠 <zhangqixcu@gmail.com>
2021-07-16 22:06:44 +0800

    edit README.md

--- Example/AppUpdate/AppDelegate.swift
+++ Example/AppUpdate/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        UpdateProvider.shared.updateVersion(update_url: "https://app.jushuitan.com/sdrms/checkversion", channel: "distrbution")
+        UpdateProvider.shared.updateVersion(update_url: "https://xxxxxx", channel: "distrbution")
 
         return true
     }
